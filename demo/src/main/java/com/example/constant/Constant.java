9f8293ef5684945bc66fca108ae8a41f9da3772f
lengweijian@163.com <lengweijian@163.com>
2019-03-02 20:38:49 +0800

    add  interface2

--- demo/src/main/java/com/example/constant/Constant.java
+++ demo/src/main/java/com/example/constant/Constant.java
@@ -17,10 +17,21 @@ public class Constant {
      *  接口1 :查询全部
      *
      */
-    public static final String INTERFACE1_FINDALL = "http://localhost:12345/findAll";
+    public static final String INTERFACE1_FINDALL = "http://localhost:12345/RepayPlanTrial/findAll";
 
     /**
      *  接口1 : 添加一条
      */
-    public static final String INTERFACE1_ADDONE = "http://localhost:12345/addOne";
+    public static final String INTERFACE1_ADDONE = "http://localhost:12345/RepayPlanTrial/addOne";
+
+    /**
+     *  接口2 :查询全部
+     *
+     */
+    public static final String INTERFACE2_FINDALL = "http://localhost:12345/RepayPlanOFLoan/findAll";
+
+    /**
+     *  接口2 : 添加一条
+     */
+    public static final String INTERFACE2_ADDONE = "http://localhost:12345/RepayPlanOFLoan/addOne";
 }
